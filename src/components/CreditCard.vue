5f818787c359026d06f454a60bd285fa195a1790
storyofsoft <storyofsoft1024@gmail.com>
2022-02-22 14:00:24 +0100

    update

--- src/components/CreditCard.vue
+++ src/components/CreditCard.vue
@@ -1,17 +1,25 @@
 <template>
-  <form>
+  <div>
     <fieldset class="card-container">
       <legend>Credit Card Information</legend>
       <div class="card-number">
         <img :src="ccType ? require('../assets/' + ccType + '.png') : require('../assets/front.png')" alt="card">
-        <input type="text" :value="formatCardNumber" @input="handleChange" name="card" id="card" pattern="[0-9]*" placeholder="xxxx xxxx xxxx xxxx" class="effect">
-        <input v-if="ccType" :value="formatExpiration" @input="handleExpiration" placeholder="MM/YY"  maxlength="5" pattern="[0-9]*" type="text" class="card-expiration" id="card-expiration">
-        <input v-if="ccType" placeholder="CVV" maxlength="3"  pattern="[0-9]*" type="text" class="card-cvv" id="card-cvv">
-        <input v-if="ccType" placeholder="ZIP" maxlength="5" pattern="[0-9]*" type="text" class="card-zip" id="card-zip">
+        <input v-if="inputAlert" type="text" :value="formatCardNumber.substr(15)" @input="handleChange" :class="ccType?'slide-fade-enter-active':''" name="card" id="card" pattern="[0-9]*" placeholder="xxxx xxxx xxxx xxxx" class="effect">
+        <input v-else type="text" :value="formatCardNumber" @input="handleChange" name="card" id="card" pattern="[0-9]*" placeholder="xxxx xxxx xxxx xxxx" class="effect">
+        <input v-if="inputAlert" :value="formatExpiration" @input="handleExpiration" placeholder="MM/YY"  maxlength="5" name="expiration" pattern="[0-9]*" type="text" class="card-expiration" id="expiration">
+        <input v-if="inputAlert" :value="ccv" @input="handleCvv" placeholder="CVV" @keypress="imageChange" maxlength="3" pattern="[0-9]*" type="text" class="card-cvv" id="card-cvv">
+        <input v-if="inputAlert" :value="zip" @input="handleZip" placeholder="ZIP" maxlength="5" pattern="[0-9]*" type="text" class="card-zip" id="card-zip">
       </div>
     </fieldset>
-    <div>Please enter your credit card number</div>
-  </form>
+    <div v-if="!successAlert">
+      <div v-if="!ccType">
+        <div v-if="!wrongNumberAlert">Please enter your credit card number</div>
+        <div v-else class="errorColor">Please enter a valid credit card number</div>
+      </div>
+      <div v-else>Please enter your card's expiration month and year</div>
+    </div>
+    <div v-else class="success">Hooray! You've successfully filled out your credit card information.</div>
+  </div>
 </template>
 <script>
 const ccDefinitions = {
@@ -24,23 +32,23 @@ export default {
   components: {
 
   },
-
-  data() {
-    return {
-      cardNumber: '',
-      expiration: '',
-      ccv: '',
-      zip: '',
-      ccType : ''
-    };
-  },
+  data: () => ({
+    cardNumber: '',
+    expiration: '',
+    ccv: '',
+    zip: '',
+    ccType : '',
+    wrongNumberAlert: false,
+    successAlert: false,
+    inputAlert: false,
+  }),
   computed: {
     formatCardNumber() {
       return this.cc_format(this.cardNumber).toString()
     },
     formatExpiration() {
       return this.ce_format(this.expiration).toString()
-    }
+    },
   },
   methods: {
     cc_format(value) {
@@ -69,14 +77,47 @@ export default {
       }
       return item
     },
-    
     handleChange(e) {
       this.cardNumber = e.target.value
-      console.log("hello", this.cardNumber)
-      console.log(this.getCreditCardType(this.cardNumber))
+      console.log("this.cardNumber.length ", this.cardNumber.length )
+      console.log("this.getCreditCardType(this.cardNumber)", this.getCreditCardType(this.cardNumber))
+      if(this.cardNumber.length == 19 && !this.getCreditCardType(this.cardNumber))
+      {
+        this.wrongNumberAlert = true
+      }
+      if(this.cardNumber.length == 19 && this.getCreditCardType(this.cardNumber))
+      {
+        this.inputAlert = true
+        setTimeout(() => {this.getFocus("expiration")}, 100);
+      }
     },
     handleExpiration(e) {
       this.expiration = e.target.value
+      if(this.expiration.length == 5)
+      {
+        this.getFocus("card-cvv")
+      }
+    },
+    handleCvv(e) {
+      console.log("e.target.value", e.target.value)
+      this.ccv = e.target.value
+      if(this.ccv.length == 3)
+      {
+        this.getFocus("card-zip")
+        console.log("this.ccv", this.ccv)
+      }
+    },
+    handleZip(e) {
+      this.zip = e.target.value
+      console.log("this.zip", this.zip)
+      if(this.zip.length == 5)
+      {
+        this.successAlert = true
+      }
+    },
+    getFocus(id) {
+      console.log("hello", id)
+      document.getElementById(id).focus();
     },
     getCreditCardType (number) {
       for ( const [key, value] of Object.entries(ccDefinitions)) {
@@ -85,8 +126,12 @@ export default {
           this.ccType = key;
           return this.ccType;
         }
+        return false;
       }
     },
+    imageChange() {
+      this.ccType = "back"
+    }
   }
 };
 </script>
@@ -127,4 +172,11 @@ input::placeholder {
   display: flex;
   align-items: center;
 }
+.errorColor {
+  color : red;
+}
+.success {
+  color: green
+}
+
 </style>
